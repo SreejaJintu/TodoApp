f7740bf656d6586176e6d7293a7df5e23f0f2293
sreeja o s <sreejaosreenivasan@gmail.com>
2024-08-03 23:58:48 +0530

    fourth commit

--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import './ListItem.css'
 
-function ListItem({taskname,index,status,markAsDone,handleDelete}) {
+function ListItem({tasks,setTasks}) {
 
-  
-  return (
-    <div className='ListItems'>
+  const handleDelete = (index) => {
+    var message=window.confirm('Are you sure to delete?')
+    var temp1;
+    if(message){
+       temp1 = [...tasks]
+      temp1.splice(index, 1)
+      setTasks(temp1)
+    }
       
-        <h3 className='name'>{taskname}</h3>
-        <button onClick={()=>handleDelete(index)}>Delete</button>
-        <button onClick={()=>markAsDone(index)}>Mark as Done</button>
+  }
+  const markAsDone = (index) => {
+    const temp2 = [...tasks]
+    temp2[index].status = !temp2[index].status
+    console.log(temp2[index].status)
+    setTasks(temp2)
 
+
+  }
+  return (
+
+    <div className='ListItems'>
+      {tasks.map((task,i)=>(
+        <div key={i} className='items' >
+        <h3 className={`taskname ${task.status ? 'done' : ''}` }>{task.text}</h3>
+        <button onClick={()=>handleDelete(i)}>Delete</button>
+        <button onClick={()=>markAsDone(i)}>Mark as Done</button>
+</div>
+      ))}
+        
     </div>
   )
 }
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,56 +1,46 @@
 import React from 'react'
 import './Todo.css'
-import  { useState } from 'react'
+import { useState } from 'react'
 import ListItem from './ListItem'
 
 
 function Todo() {
-  const [tasks,setTasks]=useState([])
-  const [inputValue,setInputValue]=useState('')
+  const [tasks, setTasks] = useState([])
+  const [inputValue, setInputValue] = useState('')
 
-  const handleInputChange=(e)=>{
-      setInputValue(e.target.value)
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
 
   }
-  const handleSave=()=>{
+  const handleSave = () => {
 
     setTasks([...tasks, { text:inputValue, status:false }]);
 
-      setInputValue('')
+    setInputValue('')
   }
-  const handleDelete=(index)=>{
-    const temp1=[...tasks]
-    temp1.splice(index,1)
-    setTasks(temp1)
-  }
-  const markAsDone=(index)=>{
-    const temp2=[...tasks]
-    temp2[index].status=true
-    console.log(temp2[index].status)
-    setTasks(temp2)
 
-
-  }
   return (
-    
-      <div className="Todo-container">
+
+    <div className="Todo-container">
       <h1>ToDo App</h1>
       <div className=' top_container'>
-        
-        <input type='text' id='taskInput'onChange={handleInputChange} placeholder='Add a new task' value={inputValue}/>
+
+        <input type='text' id='taskInput' onChange={handleInputChange} placeholder='Add a new task' value={inputValue} />
         <button onClick={handleSave}>Add Task</button>
       </div>
-      <div  className=' bottom_container'>
-      
-       {tasks.map((element,index)=>{return <ListItem taskname={element.text} index={index} key={index}  markAsDone={markAsDone} handleDelete={handleDelete}/>})}
-      
-      </div>
-      
+      <div className=' bottom_container'>
+
+        
+           <ListItem tasks={tasks} setTasks={setTasks} /> 
       
 
-      
+      </div>
+
+
+
+
     </div>
-    
+
   )
 }
 
